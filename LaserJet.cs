4c2b9ce9a2ed6b0d10a130ed32163cfa6af55007
Muhammad Agung Wicaksonosidi <58759829+magung769@users.noreply.github.com>
2020-05-06 08:00:08 +0700

    Tugas Lab 5 - OOP Polymorphism

---
+++ LaserJet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class LaserJet : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Laser Jet display dimension: 12*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Laser Jet printer printing");
+        }
+    }
+}
